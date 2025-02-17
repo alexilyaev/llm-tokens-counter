58ee18bb3297621497c79a711997d910da1b270d
Alex Ilyaev <alexilyaev@gmail.com>
2025-02-17 13:08:30 +0200

    Transpile test files as well

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { builtinModules } from 'node:module';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import { globSync } from 'tinyglobby';
 import { readNodeVersion } from './infra/readNodeVersion';
 
 const externalPackagesRegex = /^[^./]/;
-
 const nodeVersion = await readNodeVersion();
+const cwd = process.cwd();
+const entryFilePath = path.resolve(cwd, 'src/extension.ts');
+const testFilePaths = globSync(path.resolve(cwd, 'src/**/*.spec.ts'));
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(process.cwd(), 'src', 'extension.ts'),
-      name: 'extension',
-      fileName: 'extension',
+      entry: [entryFilePath, ...testFilePaths],
       formats: ['cjs'],
     },
     rollupOptions: {
